9adce18e2a8ffaab5fa3aaf103c5204c9a94ab67
Jordan <jordanogilvy@live.com>
2023-03-05 18:23:52 +1300

    add User authentication

---
+++ Migrations/20230305050457_UserKeyRename.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserKeyRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_Userid",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Userid",
+                table: "Characters",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_Userid",
+                table: "Characters",
+                newName: "IX_Characters_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Characters",
+                newName: "Userid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters",
+                newName: "IX_Characters_Userid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_Userid",
+                table: "Characters",
+                column: "Userid",
+                principalTable: "Users",
+                principalColumn: "id");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,7 @@ namespace dotnet_rpg.Models
 {
     public class User
     {
-        public Guid id {get; set;}
+        public Guid Id {get; set;}
         public string Username {get; set;} = string.Empty;
         public byte[] PasswordHash {get; set;} = new byte[0];
         public byte[] PasswordSalt {get; set;} = new byte[0];
